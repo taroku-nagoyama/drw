e2acf9042f927bf904c773023a538d6903c3e3d7
taroku-nagoyama <taroku.nagoyama@gmail.com>
2019-04-27 14:35:46 -0400

    test2

---
+++ _scripts/takemeback.py
@@ -0,0 +1,102 @@
+import sys, os, shutil, random, re
+import __main__
+import getopt
+from datetime import datetime  
+from datetime import timedelta  
+import subprocess
+
+# .... Get command line arguments and options with getopt
+# .... Example: blah.py -min optMin -max optMax
+optlist, args = getopt.getopt(sys.argv[1:], "m:x:", ["min=", "max="])
+optlistDict = dict(optlist)
+# print(optlistDict)
+optMin = 30 if '--min' not in optlistDict else int(optlistDict['--min'])
+optMax = 60 if '--max' not in optlistDict else int(optlistDict['--max'])
+timePassedMin = random.randrange(optMin) + (optMax - optMin)
+# print('opts:', optMin, optMax)
+print('Delta:', timePassedMin)
+
+
+def parse_iso8601(sss):
+	# LINK: https://regex101.com/r/Xz0oia/1
+	sss = re.sub(r"(\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d-\d\d):(\d\d)", "\\1\\2", sss)
+	parsed = datetime.strptime(sss, '%Y-%m-%dT%H:%M:%S%z')
+	return parsed
+
+def getLastGitCommitDateIso8601Str():
+	#test = subprocess.Popen('git log -1 --format="%at" | xargs -I{} date -d @{} "+%Y-%m-%dT%H:%M:%SZ"', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	gitlogLastDate = subprocess.Popen('git log -1 --format="%cI"', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	gitlogLastDate_out, gitlogLastDate_err = gitlogLastDate.communicate()
+	# print(gitlogLastDate_out.strip())
+	# print(gitlogLastDate_err)
+	if len(gitlogLastDate_err) > 0:
+		print('Error getting last commit date:')
+		print(gitlogLastDate_err.decode("utf-8").strip())
+		sys.exit(1)
+	return gitlogLastDate_out.decode("utf-8").strip()
+
+def MyMainProg():
+	random.seed()
+
+	lastDateStr = getLastGitCommitDateIso8601Str()
+	if lastDateStr == '':
+		lastDateStr = '2018-07-29T14:16:13-04:00'
+	lastDateObj = parse_iso8601(lastDateStr)
+	print('Last date:', lastDateObj)
+
+	newDateObj = lastDateObj + timedelta(minutes=timePassedMin, seconds=random.randrange(59) + 1)
+	print('New date: ', newDateObj)
+
+	##----------------------- above this line, extract + recompute -----------------------------------
+
+	# dateProc_cmdStr = "date " +  newDateObj.strftime("%m/%d/%Y")
+	dateProc_cmdStr = "date " +  str(newDateObj.date())
+	timeProc_cmdStr = "time " +  str(newDateObj.time())
+	# timeProc_cmdStr = "time " +  newDateObj.strftime("%H:") # date format ref LINK: http://strftime.org/
+	# print('Command:', dateProc_cmdStr)
+	# print('Command:', timeProc_cmdStr)
+
+	# .... write string to batch file (UTF-8)
+	# outputBatFolder = os.environ['TEMP'] + '\\ubliabl' # NOTE: TEMP keeps getting wiped...
+	outputBatFolder = os.environ['LocalAppData'] + '\\ubliabl'
+	if not os.path.exists(outputBatFolder):
+		print ('make dir: ' + outputBatFolder)
+		os.makedirs(outputBatFolder)
+	# .... make a dir for output file(s) if it does not exista
+	batFileOut = open(outputBatFolder + '\\camel-with-3-humps.bat', "w", encoding="utf8")
+	batFileOut.write("start ms-settings:dateandtime\n" + dateProc_cmdStr + '\n' + timeProc_cmdStr)
+	batFileOut.close()
+
+	subprocess.Popen('explorer "' + outputBatFolder + '"')
+
+	print ('You now have to run camel-with-3-humps.bat as Administrator')
+
+	# dateProc_handle = subprocess.Popen(dateProc_cmdStr, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	# dateProc_stdout, dateProc_stderr = dateProc_handle.communicate()
+	# print (dateProc_stdout)
+	# print (dateProc_stderr)
+
+	# timeProc_handle = subprocess.Popen(timeProc_cmdStr, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	# timeProc_stdout, timeProc_stderr = timeProc_handle.communicate()
+	# print (timeProc_stdout)
+	# print (timeProc_stderr)
+
+	# input("Press Enter to continue...")
+
+MyMainProg()
+
+# TODO: wip, this runs too quickly and we dont see the errors
+
+# import ctypes, sys # NOTE: delete "sys" if sys is already imported
+# # LINK: https://stackoverflow.com/questions/130763/request-uac-elevation-from-within-a-python-script
+# def is_admin():
+#     try:
+#         return ctypes.windll.shell32.IsUserAnAdmin()
+#     except:
+#         return False
+
+# if is_admin():
+# 	MyMainProg()
+# else:
+#     # Re-run the program with admin rights
+#     ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
